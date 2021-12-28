e60376d5c954af2d1ca6af719dd87f4c0f23d9ee
TolikaTolika123 <tolikatolika794@gmail.com>
2021-12-28 12:24:05 +0400

    rewrited constructor into class

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ const read = document.querySelector('#read');
 const books = document.querySelector('.books')
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read ? 'Read' : 'Not read';
+class Book {
+
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  get read() {
+    return this._read;
+  }
+
+  set read(value) {
+    if (value) {
+      this._read = 'Read';
+    } else if (!value) {
+      this._read = 'Not read';
+    }
+  }
 }
 
 function addBooksToLibrary(title, author, pages, read) {
@@ -32,19 +47,23 @@ function build() {
 }
 
 function del(id) {
-  myLibrary.splice(id ,1);
+  myLibrary.splice(id, 1);
   document.getElementById('arrayPos-' + id).className = 'del';
   setTimeout(build, 100);
 }
 
 createBtn.addEventListener('click', () => {
   if (title.vaule != '' && author.value != '' && pages.value != '0') {
-  addBooksToLibrary(title.value, author.value, pages.value, read.checked);
-  build();
+    addBooksToLibrary(title.value, author.value, pages.value, read.checked);
+    build();
   }
 })
 
 function edit(id) {
-  myLibrary[id].read = myLibrary[id].read == 'Read' ? 'Not read' : 'Read'; 
+  if (myLibrary[id]._read == 'Read') {
+    myLibrary[id]._read = 'Not read'
+  } else {
+    myLibrary[id]._read = 'Read';
+  }
   build();
 }
